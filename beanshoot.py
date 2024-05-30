379233bfd4d1ccc4379b409138d051cee5f79e6b
ytqlq <ytqlq@sina.com>
2024-05-30 21:28:33 +0800

    增加图片切换功能

--- beanshoot.py
+++ beanshoot.py
@@ -16,6 +16,7 @@ maindir = os.path.split(os.path.abspath(__file__))[0]
 def load_image(img_name, scale=(75, 75)):
     img_path = os.path.join(maindir, "pics", img_name)
     imgsurf = pygame.image.load(img_path)  # .convert()
+    
     return pygame.transform.scale(imgsurf, scale)
 
 
@@ -51,15 +52,18 @@ def firebean(start_x, start_y, rect_zombie, vo, angle, block_rect, speedadd=40):
 
 screen = pygame.display.set_mode((screen_width, screen_height))
 pygame.display.set_caption("Bean Shoot")
-img_body = load_image("beanbody.png")
-img_head = load_image("beanhead.png")
+# img_body = load_image("beanbody.png")
+# img_head = load_image("beanhead.png")
 img_zombie = load_image("zoombie.png", (150, 150))
 img_grass = load_image("grass.png")
 img_grass = pygame.transform.scale(img_grass, (screen_width, 50))
 img_bullet = load_image("bullet.png")# todo 
 img_bullet = pygame.transform.scale(img_bullet,(20,20))
 # img_bullet = pygame.Surface((10,10))
-
+beans_ind_list = [0,15,30,45,60,75,85]
+dic_img_bean = dict()
+for bean_angle in beans_ind_list:
+    dic_img_bean[bean_angle] = load_image(f"beans/bean{bean_angle}.png",(80,108))
 
 
 
@@ -172,10 +176,18 @@ class Text:
         self.text = text
         self.updatesurf()
 
+class Bean(gameObject):
+    def __init__(self, image_surf, image_rect, boundary_left, boundary_right, *groups) -> None:
+        super().__init__(image_surf, image_rect, boundary_left, boundary_right, *groups)
+        
+    def updateimage(self, image):
+        self.image_surf = image
+        
 
 def gameloop():
     fps = 10
-    bean_rect = img_head.get_rect(x=0, y=screen_height - 100)    
+    bean_rect = dic_img_bean[0].get_rect(x=0, y=screen_height - 120)  
+      
     rect_zombie = img_zombie.get_rect(x=screen_width - 150, y=screen_height - 150)
     rect_zombie_shrink = rect_zombie.inflate(-80, -10)
     rect_grass = img_grass.get_rect(bottom=screen_height)
@@ -186,8 +198,8 @@ def gameloop():
     angle = 0
     block_wall = wall(bean_rect, rect_zombie)
     direct = None
-    bean_body = gameObject(img_body, bean_rect, 0, block_wall.x)
-    bean_head = gameObject(img_head, bean_rect, 0, block_wall.x)
+    bean_0 = Bean(dic_img_bean[angle], bean_rect, 0, block_wall.x)
+    # bean_head = gameObject(img_head, bean_rect, 0, block_wall.x)
     
     # rect_bullet = pygame.draw.circle(img_bullet,"green",img_bullet.get_rect().center,5)
     zombie_role = gameObject(
@@ -202,7 +214,8 @@ def gameloop():
     bullets = []
     while gamerun:
         # start_x, start_y = bean_head.image_rect.center
-        rect_bullet = img_bullet.get_rect(center = bean_head.image_rect.center)
+        old_angle = angle
+        rect_bullet = img_bullet.get_rect(center = bean_0.image_rect.center)
         rect_bullet.move_ip(0,-20)
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
@@ -216,25 +229,29 @@ def gameloop():
                     angle -= 15
                     if angle < 0:
                         angle = 0
+                    if angle == 70:
+                        angle = 75
                 elif event.key == pygame.K_LEFT:
                     direct = "left"                    
                 elif event.key == pygame.K_RIGHT:
                     direct = "right"                    
                 elif event.key == pygame.K_a:
                     firepower -= 10
-                    firepowershow.updatetext("firepower:%d" % firepower)
+                    firepowershow.updatetext("firepower:%d" % firepower) 
                 elif event.key == pygame.K_d:
                     firepower += 10
                     firepowershow.updatetext("firepower:%d" % firepower)
             if event.type == pygame.KEYUP:
                 if direct is not None:
-                    bean_head.move(direct)
-                    bean_body.move(direct)
+                    bean_0.move(direct)
+                    # bean_body.move(direct)
                     direct = None
                 if event.key == pygame.K_SPACE:                
                     # b = Bullet(img_bullet,rect_bullet.copy(),0,screen_width)
                     bullets.append(Bullet(img_bullet,rect_bullet.copy(),angle/180 * math.pi,0,screen_width))
                     # fire = True
+        if angle != old_angle:
+            bean_0.updateimage(dic_img_bean[angle])
         if zombie_role.move(zombie_direct):
             tmp = zombie_direct
             zombie_direct = zombie_opposite_direct
@@ -260,8 +277,8 @@ def gameloop():
         
         screen.blit(img_grass, rect_grass)
         block_wall.draw()
-        bean_body.draw()
-        bean_head.draw()
+        bean_0.draw()
+        # bean_head.draw()
         zombie_role.draw()
         firepowershow.write(screen, (10, 10))
         score_show.write(screen,(screen_width-score_show.fontsurf.get_width(),10))
